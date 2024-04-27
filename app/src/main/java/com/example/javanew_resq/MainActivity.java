68017f749c41cdd0e1cf7e66a2c84350cf87811e
sunnyy-serv <154304883+project-ceci@users.noreply.github.com>
2024-04-27 23:01:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/javanew_resq/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.javanew_resq;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    Button button;
+    Button button2;
+    TextView textView;
+    FirebaseUser user;
+    BottomSheetDialog sheetDialog;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.logout);
+        button2 = findViewById(R.id.button2);
+        textView = findViewById(R.id.user_details);
+        user = auth.getCurrentUser();
+        if (user == null) {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        } else {
+            textView.setText(user.getEmail());
+        }
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                sheetDialog=new BottomSheetDialog(MainActivity.this,R.style.BottomSheetStyle);
+
+                View view= LayoutInflater.from(MainActivity.this).inflate(R.layout.bottomsheet_dialog,
+                        (LinearLayout)findViewById(R.id.sheet));
+
+                sheetDialog.setContentView(view);
+
+                sheetDialog.show();
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+    }
+
+}
